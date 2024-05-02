7e6bd9703d172a3fe80db276302940ee49753042
hugovlc <hugovilaca.17@gmail.com>
2024-05-02 21:56:03 +0200

    Create attribution.js

---
+++ models/attribution.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const attributionSchema = new mongoose.Schema({
+    id_utilisateur: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Utilisateur',
+        required: true
+    },
+    id_materiel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Materiel',
+        required: true
+    },
+    date_attribution: {
+        type: Date,
+        required: true
+    },
+    date_retour_prevue: {
+        type: Date,
+        required: true
+    },
+    statut: {
+        type: String,
+        enum: ['en cours', 'terminé'],
+        default: 'en cours'
+    }
+});
+
+const Attribution = mongoose.model('Attribution', attributionSchema);
+
+export default Attribution;
