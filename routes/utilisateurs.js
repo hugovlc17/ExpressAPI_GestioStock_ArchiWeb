910a1eb90e803b98651583493103f05357a40ec4
hugovlc <hugovilaca.17@gmail.com>
2024-05-25 16:11:48 +0200

    auth sur delete Utilisateur

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.post('/',utilisateurController.createUtilisateur);
 router.post('/login', utilisateurController.loginUtilisateur);
-router.get('/',auth, utilisateurController.getAllUtilisateur);
-router.get('/:id',auth, utilisateurController.getUnUtilisateur);
-router.put('/:id',auth, utilisateurController.updateUtilisateur);
-router.delete('/:id', utilisateurController.deleteUtilisateur);
+router.get('/', auth, utilisateurController.getAllUtilisateur);
+router.get('/:id', auth, utilisateurController.getUnUtilisateur);
+router.put('/:id', auth, utilisateurController.updateUtilisateur);
+router.delete('/:id', auth, utilisateurController.deleteUtilisateur);
 
 export default router;
